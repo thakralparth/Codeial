c72ebd0cb4346c321528c29c7389889bf45b8c10
Parth Thakral <parth.thakral72@gmail.com>
2021-10-18 10:52:50 +0530

    Added Layouts and linked MongoDB

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const router=express.Router();
+const usersController=require('../controllers/users_controller');
+
+// console.log('router loaded');
+router.get('/profile',usersController.profile);
+
+module.exports=router;
