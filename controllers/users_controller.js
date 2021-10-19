2a1b6928e2d938471b2768a3581fc3ae8b480c45
Parth Thakral <parth.thakral72@gmail.com>
2021-10-20 00:48:00 +0530

    user sign in on manual-local-auth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,9 +2,24 @@ const User=require('../models/users');
 
 module.exports.profile=function(req,res){
     // res.end('<h1>User Profile</h1>');
-    res.render('user_profile',{
-        title:'Profile Page ...'
-    });
+
+    // res.render('user_profile',{
+    //     title:'Profile Page ...'
+    // });
+
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(user){
+                return res.render('user_profile',{
+                    title:"User Profile",
+                    user:user
+                })
+            }
+            return res.redirect('/users/sign-in');
+        })
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 }
 
 //render the signup Page
@@ -49,6 +64,29 @@ module.exports.create=function(req,res){
 
 //SignIn and create session for user
 module.exports.createSession=function(req,res){
-    // TODO later 
+    //steps to authenticate
+    // 1 find the user
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log('Error in finding user in Signing In');
+            return;
+        }
+        //handle user found
+        if(user){
+            //handle password which doesnot match
+            if(user.password!=req.body.password){
+                return res.redirect('back');
+            }
+
+            //handle cookie creation
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+        }else{
+            
+            //handle user not found
+            return res.redirect('back');
+        }
+
+    });
 }
 
