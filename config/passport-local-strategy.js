8e1730756d3455335d5b8b0760004e62c9d8d60f
Parth Thakral <parth.thakral72@gmail.com>
2021-10-31 18:26:50 +0530

    Passing user data to views and restricting page access(signup/signin)

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -53,6 +53,7 @@ passport.setAuthenticatedUser=function(req,res,next){
     if(req.isAuthenticated()){
         //req.user contains the current signed in user from the session cookie and we are just sending 
         //this to the locals for the views
+        // console.log(req.user);
         res.locals.user=req.user;
     }
     next();
