16ba5a4c2bac65d9fc488f1e99341ac92fa8874d
Parth Thakral <parth.thakral72@gmail.com>
2021-11-07 01:41:08 +0530

    Display posts and related user

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,8 +1,28 @@
+const Post=require('../models/post');
+
 module.exports.home=function(req,res){
     // return res.end('<h1>Express is up for Codeial</h1>')
-    console.log(req.cookies);
-    res.cookie('user_id',25);
-    return res.render('home',{
-        title:"Home"
-    });
+
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
+
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title:"Codeial | Home",
+    //         posts:posts
+    //     });
+    // })
+
+
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Codeial | Home",
+            posts:posts
+        });
+    })
+
+    // return res.render('home',{
+    //     title:"Home"
+    // });
 }
