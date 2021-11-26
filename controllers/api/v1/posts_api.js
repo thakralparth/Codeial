e2223488671debd05c9c88d86fa78960cadfb0e9
Parth Thakral <parth.thakral72@gmail.com>
2021-11-27 04:17:30 +0530

    Authentication and Authorization

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -24,23 +24,25 @@ module.exports.destroy=async function(req,res){
     try{
         let post=await Post.findById(req.params.id);
         // .id means converting the object id to string (instead of ._id)
-    
-        post.remove();
+        if(post.user==req.user.id){
+            post.remove();
 
         await Comment.deleteMany({post:req.params.id});  //query for deleting comments of post:id of that post
-
-        
-
-
-        // req.flash('success','Post and its associated comments removed');  
+         // req.flash('success','Post and its associated comments removed');  
         return res.json(200,{
             message:"Post and associated comments deleted successfully"
-        })
+        });
+        }else{
+            return res.json(401,{
+                message:"You cannot delete this post"
+            })
+        }
+        
         
     
     }catch(err){
         
-        returnres.json(500,{
+        return res.json(500,{
             message:"Internal server error"
         });
     }
